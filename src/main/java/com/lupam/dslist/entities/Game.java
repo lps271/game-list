9e9ba46ba5675a84848d975f3df456008b6d168b
lps271 <lpslucas27@gmail.com>
2024-11-12 21:37:45 -0300

    ORM configuration of the game entity

--- src/main/java/com/lupam/dslist/entities/Game.java
+++ src/main/java/com/lupam/dslist/entities/Game.java
@@ -1,11 +1,19 @@
 package com.lupam.dslist.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name="tb_game")
 public class Game {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platform;
